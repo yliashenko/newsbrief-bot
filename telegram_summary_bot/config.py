df3d501cd831fc8ffc0f8653005f828b9e6f3be5
Yevhen Liashenko <149672980+yliashenko@users.noreply.github.com>
2025-05-08 00:07:39 +0300

    Update config.py

--- telegram_summary_bot/config.py
+++ telegram_summary_bot/config.py
@@ -1,6 +1,6 @@
-api_id = 26153106
-api_hash = "b1cdead49992b8d8b6a5f3b782562a00"
-bot_token = "7859911995:AAG4dxWaEEIVPiuZAM3ykE1Z54B68vAI5MY"
-openai_api_key = "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"
-chat_id = 228966067
-channel_usernames = ["@denissexy", "@imatrofAI", "@seeallochnaya", "@ppprompt"]
+API_ID=
+API_HASH=""
+BOT_TOKEN=""
+GROQ_API_KEY=""
+CHAT_ID=
+channel_usernames=["@denissexy", "@imatrofAI", "@seeallochnaya", "@ppprompt"]
