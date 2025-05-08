ac792db4aa70b09fab0e30a7c6534c58eee4389d
yliashenko <lyashenko.eugene1937@gmail.com>
2025-05-08 16:39:34 +0300

    config.py updated

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-API_ID=
-API_HASH=
-BOT_TOKEN=
-GROQ_API_KEY=
-CHAT_ID=
-channel_usernames=["@denissexy", "@imatrofAI", "@seeallochnaya", "@ppprompt"]
+import os
+
+api_id = int(os.environ["API_ID"])
+api_hash = os.environ["API_HASH"]
+bot_token = os.environ["BOT_TOKEN"]
+openai_api_key = os.environ["GROQ_API_KEY"]
+chat_id = int(os.environ["CHAT_ID"])
+channel_usernames = ["@denissexy", "@imatrofAI", "@seeallochnaya", "@ppprompt"]
