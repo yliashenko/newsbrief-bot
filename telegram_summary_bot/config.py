859a6d91d32188253bdae0fa12e0e9c787363feb
yliashenko <lyashenko.eugene1937@gmail.com>
2025-05-08 15:11:27 +0300

    add user_session file, config.py updated

--- telegram_summary_bot/config.py
+++ telegram_summary_bot/config.py
@@ -1,6 +1,6 @@
 API_ID=
-API_HASH=""
-BOT_TOKEN=""
-GROQ_API_KEY=""
+API_HASH=
+BOT_TOKEN=
+GROQ_API_KEY=
 CHAT_ID=
 channel_usernames=["@denissexy", "@imatrofAI", "@seeallochnaya", "@ppprompt"]
--- telegram_summary_bot/telegram_client.py
+++ telegram_summary_bot/telegram_client.py
@@ -1,11 +1,10 @@
 from telethon import TelegramClient
 from config import api_id, api_hash, channel_usernames
 
-# Авторизація через звичайний акаунт
 client = TelegramClient("user_session", api_id, api_hash)
 
 async def get_channel_posts(limit=10):
-    await client.start()  # ← Тут тебе попросять ввести номер телефону + код
+    await client.start()
     posts_by_channel = {}
     for channel in channel_usernames:
         messages = await client.get_messages(channel, limit=limit)
