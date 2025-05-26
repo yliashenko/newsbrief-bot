45df2b88b3a2829004afce4dc3db8833f8b88993
Liashenko, Yevhen (US - Hermitage) <yliashenko@deloitte.com>
2025-05-26 13:19:35 +0300

    No module named 'chat_bot_ui' fix

--- chat_bot_ui/bot_runner.py
+++ chat_bot_ui/bot_runner.py
@@ -1,9 +1,14 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from chat_bot_ui.bot_handlers import router
-from chat_bot_ui.bot_menu import menu_router
 from config import BOT_TOKEN
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from bot_handlers import router
+from bot_menu import menu_router
 
 async def start_bot():
     bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
