1c73367e1a976f0f0ae13666c5c6e3af7bad7e89
yliashenko <lyashenko.eugene1937@gmail.com>
2025-05-27 01:51:18 +0300

    fix

--- bot/poster.py
+++ bot/poster.py
@@ -43,5 +43,4 @@ async def send_digest_banner(category: str):
         return
 
     image = FSInputFile(image_path)
-    caption = f"<b>Що нового в: {category.upper()}</b>"
-    await bot.send_photo(chat_id=CHAT_ID, photo=image, caption=caption)
+    await bot.send_photo(chat_id=CHAT_ID, photo=image)
